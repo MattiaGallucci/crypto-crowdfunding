4411075211bf09d7358f759436a078c5456b58df
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-11-23 15:02:42 +0100

    inizializzazione front end con vite e test avvio

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Crypto Crowdfunding</h1>
+      <p>DApp in costruzione...</p>
+    </div>
+  );
+}
+
+export default App;
